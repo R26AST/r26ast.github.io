2b82476daaa111b8576439e11aa4d8c5752b66a3
R26 <95674004+R26AST@users.noreply.github.com>
2024-08-23 17:39:51 +0700

    Add files via upload

---
+++ assets/js/mail.js
@@ -0,0 +1,18 @@
+/*
+let nama = $('#name').val(),
+email = $('#email').val(),
+subjek = $('#subject').val(),
+pesan = $('#message').val();
+
+$('#kirim').click(function () {
+    Email.send({
+        SecureToken : "ad9fc324-a7c9-4513-b661-aeb97ae827a5",
+        To : 'putrakuruliff1980@gmail.com',
+        From : nama + email,
+        Subject : subjek,
+        Body : pesan
+    }).then(
+      message => alert(message)
+    );
+});
+*/
